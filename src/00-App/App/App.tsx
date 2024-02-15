7e32d5f4447de766f7dfba23b35afcc0f421e7dc
agro40tech <volkovvova67@gmail.com>
2024-02-15 17:18:31 +0400

    Настроил Redux-tool-kit

--- src/00-App/App/App.tsx
+++ src/00-App/App/App.tsx
@@ -1,7 +1,27 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 
 import "../style/index.css";
+import { Button } from "@mui/material";
+import { increment, useAppDispatch, useAppSelector } from "05-Shared";
 
 export const App: FC = () => {
-  return <div>1</div>;
+  const count = useAppSelector((state) => state.test.count);
+  const dispath = useAppDispatch();
+
+  useEffect(() => {
+    console.log(count);
+  }, [count]);
+
+  const clickHandle = () => {
+    dispath(increment(1));
+  };
+
+  return (
+    <div>
+      <Button variant="contained" onClick={clickHandle}>
+        Добавить 1
+      </Button>{" "}
+      {count}
+    </div>
+  );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,16 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+import { Provider } from "react-redux";
 import { App } from "00-App";
+import { store } from "05-Shared";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
