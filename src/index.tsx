e9559c68fc67cb4d2b6ee838a4a72da29c1a9836
agro40tech <volkovvova67@gmail.com>
2024-02-15 15:16:28 +0400

    Обновил зависимости проекта

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 
+// Шрифты для библиотеки Material UI
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import { App } from "00-App";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
